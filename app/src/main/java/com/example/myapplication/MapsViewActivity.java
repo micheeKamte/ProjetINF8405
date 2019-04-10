33409d7de5af0d2057229b9b860ec2ea8cebfa1f
micheeKamte <micheekamte@gmail.com>
2019-04-10 01:56:51 -0400

    implementation des capteurs

--- app/src/main/java/com/example/myapplication/MapsViewActivity.java
+++ app/src/main/java/com/example/myapplication/MapsViewActivity.java
@@ -1,27 +1,127 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsViewActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsViewActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,
+        View.OnClickListener {
 
     private GoogleMap mMap;
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    protected LocationManager locationManager;
+    private double latitude, longitude;
+
+    private Button refresh, swap;
+
+    private static final int LOCATION_REQUEST_CODE =101;
+
+    private String nom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_maps_view);
+
+        currentLocation = null;
+
+        nom = "Merci";
+
+        refresh = (Button) findViewById(R.id.btn_refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SensorActivity.class);
+                startActivity(intent);
+                /*
+                latitude = currentLocation.getLatitude();
+                longitude = currentLocation.getLongitude();
+                onMapReady(mMap);
+                */
+            }
+        });
+
+        swap = (Button) findViewById(R.id.btn_swap);
+        swap.setOnClickListener(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(MapsViewActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(MapsViewActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MapsViewActivity.this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fetchLastLocation();
+    }
+
+    /***
+     * Obtenir les coordonnées de notre appreils tout en verifiant si l'application a obtenu la permission
+     */
+    private void fetchLastLocation(){
+        if (ActivityCompat.checkSelfPermission(MapsViewActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(MapsViewActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MapsViewActivity.this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(MapsViewActivity.this,currentLocation.getLatitude()+" "+currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(MapsViewActivity.this);
+                    latitude = currentLocation.getLatitude();
+                    longitude = currentLocation.getLongitude();
+                }else{
+                    Toast.makeText(MapsViewActivity.this,"No Location recorded",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
@@ -37,10 +137,72 @@ public class MapsViewActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMinZoomPreference(10);
+
+
+        LatLng position = new LatLng(latitude, longitude);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(position)
+                .title("Position" + "\n" + nom)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        Marker m = mMap.addMarker(markerOptions);
+        m.showInfoWindow();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Toast.makeText(getApplicationContext(), "Position" + latitude + longitude, Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    /***
+     * Obtenir les permissions pour la géolocalisation
+     * @param requestCode
+     * @param permissions
+     * @param grantResult
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResult) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLastLocation();
+                } else {
+                    Toast.makeText(MapsViewActivity.this,"Location permission missing",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /***
+     * changer de theme par un click sur le bouton swap
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        if(Utils.isActive == true){
+            Utils.changeToTheme(this, Utils.THEME_BLACK);
+        } else {
+            Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SensorActivity.java
@@ -0,0 +1,109 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    private Sensor sPressure, sLight, sTemperature;
+
+    TextView light_tv, pressure_tv, temperature_tv, longitude_tv, latitude_tv;
+
+    Button saveInfo_bt, getInfo_bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        light_tv = (TextView)findViewById(R.id.light);
+        pressure_tv = (TextView)findViewById(R.id.pressure);
+        temperature_tv = (TextView)findViewById(R.id.temperature);
+
+        longitude_tv = (TextView)findViewById(R.id.longitude);
+        longitude_tv.setText("Longitude : ...");
+
+        latitude_tv = (TextView)findViewById(R.id.latitude);
+        latitude_tv.setText("Latitude : ...");
+
+        /***
+         * Obtenir une instance du service de capteur, et l'utiliser pour obtenir
+         * des instances des differents capteurs
+         */
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        /***
+         * Obtenir les instances des capteurs désirées
+         */
+        sPressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if(sPressure == null){
+            pressure_tv.setText("La pression n'est pas supportée");
+        }
+
+        sLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(sLight == null){
+            light_tv.setText("La luminosité n'est pas supportée");
+        }
+
+        sTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if(sTemperature == null){
+            temperature_tv.setText("La température n'est pas supportée");
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if(sensor.getType() == Sensor.TYPE_PRESSURE) {
+            pressure_tv.setText("La pression est : " + event.values[0]);
+        } else if (sensor.getType() == Sensor.TYPE_LIGHT) {
+            light_tv.setText("La luminosité est : " + event.values[0]);
+        } else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            temperature_tv.setText("La temperature est : " + event.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        /***
+         * Arreter l'activité du capteur quand l'activité est en pause.
+         * eviter la surconsommation de la batterie de l'appareil
+         */
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        /***
+         * enregister les valeurs des capteurs
+         */
+        super.onResume();
+        if(sPressure != null){
+            sensorManager.registerListener(SensorActivity.this, sPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(sLight != null){
+            sensorManager.registerListener(SensorActivity.this, sLight, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(sTemperature != null){
+            sensorManager.registerListener(SensorActivity.this, sTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+    }
+}
